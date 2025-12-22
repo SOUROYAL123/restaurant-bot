36f025d1d41f18596a55fd4fa60a95de15947d71
SOUROYAL123 <souravrchowdhury2020@gmail.com>
2025-12-22 20:52:04 +0530

    Update sendMessage.js

--- utils/sendMessage.js
+++ utils/sendMessage.js
@@ -1,1 +1,9 @@
 
+// utils/sendMessage.js
+
+async function sendMessage(to, message) {
+  // 🔁 Replace this with your real WhatsApp send logic
+  console.log(`📤 Sending message to ${to}: ${message}`);
+}
+
+module.exports = { sendMessage };
